IceDev528/vuetify

commit 9c122fe71b421d0509cec745bafb7277fdf8f317

feat: use a bootstrap-style grid system (#7327)
resolves #1434
fixes #5168
resolves #5272
resolves #6020
resolves #7733

* feat: create legacy-grid package

* chore: output dev bundle

* refactor: move VForm grid overrides to legacy-grid

* feat: create new grid components

* feat: add utilities

* feat: create utility classes

* refactor: move utilities css out of component folder

* feat(VRow): add breakpoint align/justify props

* chore: remove test script

* feat: don't publish legacy-grid package

It needs some customisation to lerna to play nicely with our monorepo
setup. We can still publish manually if needed.

* refactor: use generate-utility in more places

* feat(VGrid): revert some breaking changes to classes

* fix(build): remove mqpacker

breaks ordering of grid breakpoints

* feat(grid): add horizontal gutters

* feat(spacers): use linear spacer progression

BREAKING CHANGE: all ma-x etc. helpers need to be changed

before: 0 4 8 16 24 48
after:  0 4 8 12 16 20 24 28 32 36 40 44 48

* refactor(VCol): remove redundant col prop

* fix(VCol): apply class when breakpoints specified

* feat: merge legacy-grid back into vuetify

v-layout and v-flex now coexist with the new components

some styles will still change from 1.x as there is some overlap between
the two systems, but the overall upgrade workload should be less

* docs: update classes

* feat: add back start/end spacing helpers for rtl

* feat: revert space-between -> between breaking change

* style: fix lint

* test: update snapshots

* docs: revert space-between -> between breaking change

partially reverts fc21169bc

* chore(ci): build before lint

eslint-plugin-vuetify needs to import from dist

* style: fix lint

* fix(VDataTable): replace text-xs-start

* style: fix lint

* fix(VRow): add flex-grow for IE11

packages/docs/src/components/supporters/TideliftEnterprise.vue
@@ -2,7 +2,7 @@
   <div>
     <v-responsive
       :max-width="$vuetify.breakpoint.lgAndUp ? '60%' : '100%'"
-      class="left mb-5"
+      class="left mb-12"
     >
       <doc-heading>subHeader</doc-heading>
       <doc-markdown>subText1</doc-markdown>

packages/vuetify/src/styles/settings/_utilities.scss
@@ -1,0 +1,331 @@
+$utilities: () !default;
+$utilities: map-merge(
+  (
+    // Display utilities
+    "overflow": (
+      property: overflow,
+      values: auto hidden visible,
+    ),
+    "overflow-x": (
+      property: overflow-x,
+      values: auto hidden
+    ),
+    "overflow-y": (
+      property: overflow-y,
+      values: auto hidden
+    ),
+    "display": (
+      responsive: true,
+      print: true,
+      property: display,
+      class: d,
+      values: none inline inline-block block table table-row table-cell flex inline-flex
+    ),
+
+    // Flex utilities
+    "flex": (
+      responsive: true,
+      property: flex,
+      values: (fill: 1 1 auto)
+    ),
+    "flex-direction": (
+      responsive: true,
+      property: flex-direction,
+      class: flex,
+      values: row column row-reverse column-reverse
+    ),
+    "flex-grow": (
+      responsive: true,
+      property: flex-grow,
+      class: flex,
+      values: (
+        grow-0: 0,
+        grow-1: 1,
+      )
+    ),
+    "flex-shrink": (
+      responsive: true,
+      property: flex-shrink,
+      class: flex,
+      values: (
+        shrink-0: 0,
+        shrink-1: 1,
+      )
+    ),
+    "flex-wrap": (
+      responsive: true,
+      property: flex-wrap,
+      class: flex,
+      values: wrap nowrap wrap-reverse
+    ),
+    "justify-content": (
+      responsive: true,
+      property: justify-content,
+      class: justify,
+      values: (
+        start: flex-start,
+        end: flex-end,
+        center: center,
+        space-between: space-between,
+        space-around: space-around,
+      )
+    ),
+    "align-items": (
+      responsive: true,
+      property: align-items,
+      class: align,
+      values: (
+        start: flex-start,
+        end: flex-end,
+        center: center,
+        baseline: baseline,
+        stretch: stretch,
+      )
+    ),
+    "align-content": (
+      responsive: true,
+      property: align-content,
+      values: (
+        start: flex-start,
+        end: flex-end,
+        center: center,
+        space-between: space-between,
+        space-around: space-around,
+        stretch: stretch,
+      )
+    ),
+    "align-self": (
+      responsive: true,
+      property: align-self,
+      values: (
+        auto: auto,
+        start: flex-start,
+        end: flex-end,
+        center: center,
+        baseline: baseline,
+        stretch: stretch,
+      )
+    ),
+    "order": (
+      responsive: true,
+      property: order,
+      values: (
+        first: -1,
+        0: 0,
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+        10: 10,
+        11: 11,
+        12: 12,
+        last: 13,
+      ),
+    ),
+
+    // Margin utilities
+    "margin": (
+      responsive: true,
+      property: margin,
+      class: ma,
+      values: map-merge($spacers, (auto: auto))
+    ),
+    "margin-x": (
+      responsive: true,
+      property: margin-right margin-left,
+      class: mx,
+      values: map-merge($spacers, (auto: auto))
+    ),
+    "margin-y": (
+      responsive: true,
+      property: margin-top margin-bottom,
+      class: my,
+      values: map-merge($spacers, (auto: auto))
+    ),
+    "margin-top": (
+      responsive: true,
+      property: margin-top,
+      class: mt,
+      values: map-merge($spacers, (auto: auto))
+    ),
+    "margin-right": (
+      responsive: true,
+      property: margin-right,
+      class: mr,
+      values: map-merge($spacers, (auto: auto))
+    ),
+    "margin-bottom": (
+      responsive: true,
+      property: margin-bottom,
+      class: mb,
+      values: map-merge($spacers, (auto: auto))
+    ),
+    "margin-left": (
+      responsive: true,
+      property: margin-left,
+      class: ml,
+      values: map-merge($spacers, (auto: auto))
+    ),
+    "margin-start": (
+      responsive: true,
+      property: (ltr: margin-left, rtl: margin-right),
+      class: ms,
+      values: map-merge($spacers, (auto: auto))
+    ),
+    "margin-end": (
+      responsive: true,
+      property: (ltr: margin-right, rtl: margin-left),
+      class: me,
+      values: map-merge($spacers, (auto: auto))
+    ),
+
+    // Negative margin utilities
+    "negative-margin": (
+      responsive: true,
+      property: margin,
+      class: ma,
+      values: $negative-spacers
+    ),
+    "negative-margin-x": (
+      responsive: true,
+      property: margin-right margin-left,
+      class: mx,
+      values: $negative-spacers
+    ),
+    "negative-margin-y": (
+      responsive: true,
+      property: margin-top margin-bottom,
+      class: my,
+      values: $negative-spacers
+    ),
+    "negative-margin-top": (
+      responsive: true,
+      property: margin-top,
+      class: mt,
+      values: $negative-spacers
+    ),
+    "negative-margin-right": (
+      responsive: true,
+      property: margin-right,
+      class: mr,
+      values: $negative-spacers
+    ),
+    "negative-margin-bottom": (
+      responsive: true,
+      property: margin-bottom,
+      class: mb,
+      values: $negative-spacers
+    ),
+    "negative-margin-left": (
+      responsive: true,
+      property: margin-left,
+      class: ml,
+      values: $negative-spacers
+    ),
+    "negative-margin-start": (
+      responsive: true,
+      property: (ltr: margin-left, rtl: margin-right),
+      class: ms,
+      values: $negative-spacers
+    ),
+    "negative-margin-end": (
+      responsive: true,
+      property: (ltr: margin-right, rtl: margin-left),
+      class: me,
+      values: $negative-spacers
+    ),
+
+    // Padding utilities
+    "padding": (
+      responsive: true,
+      property: padding,
+      class: pa,
+      values: $spacers
+    ),
+    "padding-x": (
+      responsive: true,
+      property: padding-right padding-left,
+      class: px,
+      values: $spacers
+    ),
+    "padding-y": (
+      responsive: true,
+      property: padding-top padding-bottom,
+      class: py,
+      values: $spacers
+    ),
+    "padding-top": (
+      responsive: true,
+      property: padding-top,
+      class: pt,
+      values: $spacers
+    ),
+    "padding-right": (
+      responsive: true,
+      property: padding-right,
+      class: pr,
+      values: $spacers
+    ),
+    "padding-bottom": (
+      responsive: true,
+      property: padding-bottom,
+      class: pb,
+      values: $spacers
+    ),
+    "padding-left": (
+      responsive: true,
+      property: padding-left,
+      class: pl,
+      values: $spacers
+    ),
+    "padding-start": (
+      responsive: true,
+      property: (ltr: padding-left, rtl: padding-right),
+      class: ps,
+      values: $spacers
+    ),
+    "padding-end": (
+      responsive: true,
+      property: (ltr: padding-right, rtl: padding-left),
+      class: pe,
+      values: $spacers
+    ),
+
+    // Text
+    "text-align": (
+      responsive: true,
+      property: text-align,
+      class: text,
+      values: left right center justify
+    ),
+    "white-space": (
+      property: white-space,
+      class: text,
+      values: (
+        wrap: normal,
+        no-wrap: nowrap,
+      )
+    ),
+    "overflow-wrap": (
+      property: overflow-wrap word-break, // word-break for IE & < Edge 18
+      class: text,
+      values: (break: break-word)
+    ),
+    "text-overflow": (
+      property: white-space overflow text-overflow,
+      class: text,
+      values: (truncate: nowrap hidden ellipsis)
+    ),
+    "text-transform": (
+      property: text-transform,
+      class: text,
+      values: none capitalize lowercase uppercase
+    )
+  ),
+  $utilities
+);

packages/vuetify/src/styles/utilities/_index.sass
@@ -1,5 +1,25 @@
 @import './_helpers.sass'
 @import './_display.sass'
 @import './_text.sass'
-@import './_spacing.sass'
 @import './_transition.sass'
+
+// Utilities
+@each $breakpoint in map-keys($grid-breakpoints)
+  // Generate media query if needed
+  +media-breakpoint-up($breakpoint)
+    $infix: breakpoint-infix($breakpoint, $grid-breakpoints)
+
+    // Loop over each utility property
+    @each $key, $utility in $utilities
+      // The utility can be disabled with `false`, thus check if the utility is a map first
+      // Only proceed if responsive media queries are enabled or if it's the base media query
+      @if type-of($utility) == "map" and (map-get($utility, responsive) or $infix == "")
+        +generate-utility($utility, $infix)
+
+// Print utilities
+@media print
+  @each $key, $utility in $utilities
+    // The utility can be disabled with `false`, thus check if the utility is a map first
+    // Then check if the utility needs print styles
+    @if type-of($utility) == "map" and map-get($utility, print) == true
+      +generate-utility($utility, "-print")

packages/vuetify/src/styles/settings/_index.sass
@@ -1,6 +1,7 @@
 @import './_variables'
 @import './_colors'
 @import './_elevations'
+@import './_utilities'
 
 // Theme specific setup
 // ordering is important

packages/vuetify/src/styles/tools/_index.sass
@@ -4,3 +4,12 @@
 @import './_rtl'
 @import './_states'
 @import './_theme'
+@import './_utilities'
+
+=media-breakpoint-up($name, $breakpoints: $grid-breakpoints)
+  $min: breakpoint-min($name, $breakpoints)
+  @if $min
+    @media (min-width: $min)
+      @content
+  @else
+    @content

commit 22e33687bfcbf3c1bfa24c8eae0f21e3518811c8

feat(styles): float classes breakpointed (#7922)
* feat(styles): float classes breakpointed

BREAKING CHANGE

removes .left and .right classes in favour of .float-{breakpont}-{dir}

* fix(style); bring back .v-application scope to utility classes (#7921)

regression from 9c122fe71b421d0509cec745bafb7277fdf8f317

* docs: fix class on tidelift page

packages/docs/src/components/supporters/TideliftEnterprise.vue
@@ -2,7 +2,7 @@
   <div>
     <v-responsive
       :max-width="$vuetify.breakpoint.lgAndUp ? '60%' : '100%'"
-      class="left mb-12"
+      class="float-left mb-12"
     >
       <doc-heading>subHeader</doc-heading>
       <doc-markdown>subText1</doc-markdown>
@@ -12,7 +12,7 @@
     </v-responsive>
 
     <v-img
-      class="right hidden-md-and-down"
+      class="float-right hidden-md-and-down"
       src="https://cdn.vuetifyjs.com/images/affiliates/vuetify-tidelift.png"
       max-width="35%"
     />

packages/vuetify/src/styles/settings/_utilities.scss
@@ -21,6 +21,13 @@ $utilities: map-merge(
       class: d,
       values: none inline inline-block block table table-row table-cell flex inline-flex
     ),
+    "float": (
+      responsive: true,
+      print: true,
+      property: float,
+      class: float,
+      values: none left right
+    ),
 
     // Flex utilities
     "flex": (

packages/vuetify/src/styles/utilities/_helpers.sass
@@ -1,6 +1,0 @@
-.v-application
-  .right
-    float: right !important
-
-  .left
-    float: left !important

packages/vuetify/src/styles/utilities/_index.sass
@@ -1,4 +1,3 @@
-@import './_helpers.sass'
 @import './_display.sass'
 @import './_text.sass'
 @import './_transition.sass'