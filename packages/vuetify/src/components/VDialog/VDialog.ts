IceDev528/vuetify

commit 2b975f575f47ba75aa5d0dd9fba859595c7b9ba4

fix(bootable): don't generate lazy content until it's rendered (#8823)
* fix(VDialog): include ThemeProvider in the lazyContent

fixes #8712

* fix(VMenu): include ThemeProvider in the lazyContent

fixes #8712

* test: update snapshots

* fix(bootable): don't generate lazy content until it's needed

* refactor(bootable): avoid rendering content wrappers before boot

* test: use eager

* fix(detachable): wait for content to render before detaching

* test(VCarouselItem): remove snapshots

* refactor: split up render functions

* refactor: store content in variable

* fix(VDialog): render transition

* test: update snapshots

packages/vuetify/src/components/VDialog/VDialog.ts
@@ -238,68 +238,68 @@ export default baseMixins.extend({
         focusable.length && (focusable[0] as HTMLElement).focus()
       }
     },
-  },
+    genTransition () {
+      const content = this.genContent()
 
-  render (h): VNode {
-    const children = []
-    const data = {
-      class: this.classes,
-      ref: 'dialog',
-      directives: [
-        {
-          name: 'click-outside',
-          value: this.onClickOutside,
-          args: {
-            closeConditional: this.closeConditional,
-            include: this.getOpenDependentElements,
-          },
-        },
-        { name: 'show', value: this.isActive },
-      ],
-      style: {},
-    }
+      if (!this.transition) return content
 
-    if (!this.fullscreen) {
-      data.style = {
-        maxWidth: this.maxWidth === 'none' ? undefined : convertToUnit(this.maxWidth),
-        width: this.width === 'auto' ? undefined : convertToUnit(this.width),
-      }
-    }
-
-    children.push(this.genActivator())
-
-    let dialog = h('div', data, this.showLazyContent(this.getContentSlot()))
-    if (this.transition) {
-      dialog = h('transition', {
+      return this.$createElement('transition', {
         props: {
           name: this.transition,
           origin: this.origin,
         },
-      }, [dialog])
-    }
+      }, this.showLazyContent(() => [content]))
+    },
+    genContent () {
+      const data = {
+        class: this.classes,
+        ref: 'dialog',
+        directives: [
+          {
+            name: 'click-outside',
+            value: this.onClickOutside,
+            args: {
+              closeConditional: this.closeConditional,
+              include: this.getOpenDependentElements,
+            },
+          },
+          { name: 'show', value: this.isActive },
+        ],
+        style: {},
+      }
 
-    children.push(h('div', {
-      class: this.contentClasses,
-      attrs: {
-        role: 'document',
-        tabindex: this.isActive ? 0 : undefined,
-        ...this.getScopeIdAttrs(),
-      },
-      on: {
-        keydown: this.onKeydown,
-      },
-      style: { zIndex: this.activeZIndex },
-      ref: 'content',
-    }, [
-      this.$createElement(VThemeProvider, {
-        props: {
-          root: true,
-          light: this.light,
-          dark: this.dark,
+      if (!this.fullscreen) {
+        data.style = {
+          maxWidth: this.maxWidth === 'none' ? undefined : convertToUnit(this.maxWidth),
+          width: this.width === 'auto' ? undefined : convertToUnit(this.width),
+        }
+      }
+
+      return this.$createElement('div', {
+        class: this.contentClasses,
+        attrs: {
+          role: 'document',
+          tabindex: this.isActive ? 0 : undefined,
+          ...this.getScopeIdAttrs(),
         },
-      }, [dialog]),
-    ]))
+        on: { keydown: this.onKeydown },
+        style: { zIndex: this.activeZIndex },
+        ref: 'content',
+      }, [
+        this.$createElement('div', data, [
+          this.$createElement(VThemeProvider, {
+            props: {
+              root: true,
+              light: this.light,
+              dark: this.dark,
+            },
+          }, this.getContentSlot()),
+        ]),
+      ])
+    },
+  },
 
+  render (h): VNode {
     return h('div', {
       staticClass: 'v-dialog__container',
       class: {
@@ -309,6 +309,9 @@ export default baseMixins.extend({
           this.attach === 'attach',
       },
       attrs: { role: 'dialog' },
-    }, children)
+    }, [
+      this.genActivator(),
+      this.genTransition(),
+    ])
   },
 })

packages/vuetify/src/components/VCarousel/VCarouselItem.ts
@@ -6,6 +6,7 @@ import { VImg } from '../VImg'
 
 // Utilities
 import mixins from '../../util/mixins'
+import { getSlot } from '../../util/helpers'
 import Routable from '../../mixins/routable'
 
 // Types
@@ -30,12 +31,10 @@ export default baseMixins.extend({
             height: this.windowGroup.internalHeight,
           },
           on: this.$listeners,
-        }, [
-          this.$slots.default,
-          this.$createElement('template', {
-            slot: 'placeholder',
-          }, this.$slots.placeholder),
-        ]),
+          scopedSlots: {
+            placeholder: this.$scopedSlots.placeholder,
+          },
+        }, getSlot(this)),
       ]
     },
     genWindowItem () {
@@ -47,7 +46,7 @@ export default baseMixins.extend({
         value: this.isActive,
       })
 
-      return this.$createElement(tag, data, this.showLazyContent(this.genDefaultSlot()))
+      return this.$createElement(tag, data, this.genDefaultSlot())
     },
   },
 })

packages/vuetify/src/components/VCarousel/__tests__/VCarouselItem.spec.ts
@@ -24,18 +24,6 @@ describe('VCarouselItem.ts', () => {
       },
     })
 
-    expect(wrapper.html()).toMatchSnapshot()
-    expect(warning).toHaveBeenTipped()
-  })
-
-  it('should render component and match snapshot', () => {
-    const wrapper = mountFunction({
-      propsData: {
-        src: imageSrc,
-      },
-    })
-
-    expect(wrapper.html()).toMatchSnapshot()
     expect(warning).toHaveBeenTipped()
   })
 })

packages/vuetify/src/components/VCarousel/__tests__/__snapshots__/VCarouselItem.spec.ts.snap
@@ -1,15 +1,0 @@
-// Jest Snapshot v1, https://goo.gl/fbAQLP
-
-exports[`VCarouselItem.ts should render component and match snapshot 1`] = `
-<div class="v-window-item"
-     style="display: none;"
->
-</div>
-`;
-
-exports[`VCarouselItem.ts should throw warning when not used inside v-carousel 1`] = `
-<div class="v-window-item"
-     style="display: none;"
->
-</div>
-`;

packages/vuetify/src/components/VDataIterator/__tests__/__snapshots__/VDataFooter.spec.ts.snap
@@ -9,7 +9,7 @@ exports[`VDataFooter.ts should disable last page button if no items 1`] = `
         <div role="button"
              aria-haspopup="listbox"
              aria-expanded="false"
-             aria-owns="list-65"
+             aria-owns="list-60"
              class="v-input__slot"
         >
           <div class="v-select__slot">
@@ -18,7 +18,7 @@ exports[`VDataFooter.ts should disable last page button if no items 1`] = `
                 10
               </div>
               <input aria-label="$vuetify.dataFooter.itemsPerPageText"
-                     id="input-65"
+                     id="input-60"
                      readonly="readonly"
                      type="text"
                      aria-readonly="true"
@@ -38,10 +38,6 @@ exports[`VDataFooter.ts should disable last page button if no items 1`] = `
             >
           </div>
           <div class="v-menu">
-            <div class="v-menu__content theme--light "
-                 style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-            >
-            </div>
           </div>
         </div>
       </div>
@@ -114,7 +110,7 @@ exports[`VDataFooter.ts should render first & last icons with showFirstLastPage 
         <div role="button"
              aria-haspopup="listbox"
              aria-expanded="false"
-             aria-owns="list-28"
+             aria-owns="list-26"
              class="v-input__slot"
         >
           <div class="v-select__slot">
@@ -123,7 +119,7 @@ exports[`VDataFooter.ts should render first & last icons with showFirstLastPage 
                 10
               </div>
               <input aria-label="$vuetify.dataFooter.itemsPerPageText"
-                     id="input-28"
+                     id="input-26"
                      readonly="readonly"
                      type="text"
                      aria-readonly="true"
@@ -143,10 +139,6 @@ exports[`VDataFooter.ts should render first & last icons with showFirstLastPage 
             >
           </div>
           <div class="v-menu">
-            <div class="v-menu__content theme--light "
-                 style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-            >
-            </div>
           </div>
         </div>
       </div>
@@ -215,7 +207,7 @@ exports[`VDataFooter.ts should render in RTL mode 1`] = `
         <div role="button"
              aria-haspopup="listbox"
              aria-expanded="false"
-             aria-owns="list-13"
+             aria-owns="list-12"
              class="v-input__slot"
         >
           <div class="v-select__slot">
@@ -224,7 +216,7 @@ exports[`VDataFooter.ts should render in RTL mode 1`] = `
                 10
               </div>
               <input aria-label="$vuetify.dataFooter.itemsPerPageText"
-                     id="input-13"
+                     id="input-12"
                      readonly="readonly"
                      type="text"
                      aria-readonly="true"
@@ -244,10 +236,6 @@ exports[`VDataFooter.ts should render in RTL mode 1`] = `
             >
           </div>
           <div class="v-menu">
-            <div class="v-menu__content theme--light "
-                 style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-            >
-            </div>
           </div>
         </div>
       </div>
@@ -345,10 +333,6 @@ exports[`VDataFooter.ts should render with custom itemsPerPage 1`] = `
             >
           </div>
           <div class="v-menu">
-            <div class="v-menu__content theme--light "
-                 style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-            >
-            </div>
           </div>
         </div>
       </div>
@@ -396,7 +380,7 @@ exports[`VDataFooter.ts should show current page if has showCurrentPage 1`] = `
         <div role="button"
              aria-haspopup="listbox"
              aria-expanded="false"
-             aria-owns="list-54"
+             aria-owns="list-50"
              class="v-input__slot"
         >
           <div class="v-select__slot">
@@ -405,7 +389,7 @@ exports[`VDataFooter.ts should show current page if has showCurrentPage 1`] = `
                 10
               </div>
               <input aria-label="$vuetify.dataFooter.itemsPerPageText"
-                     id="input-54"
+                     id="input-50"
                      readonly="readonly"
                      type="text"
                      aria-readonly="true"
@@ -425,10 +409,6 @@ exports[`VDataFooter.ts should show current page if has showCurrentPage 1`] = `
             >
           </div>
           <div class="v-menu">
-            <div class="v-menu__content theme--light "
-                 style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-            >
-            </div>
           </div>
         </div>
       </div>

packages/vuetify/src/components/VDataIterator/__tests__/__snapshots__/VDataIterator.spec.ts.snap
@@ -50,10 +50,6 @@ exports[`VDataIterator.ts should render and match snapshot 1`] = `
               >
             </div>
             <div class="v-menu">
-              <div class="v-menu__content theme--light "
-                   style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-              >
-              </div>
             </div>
           </div>
         </div>
@@ -116,7 +112,7 @@ exports[`VDataIterator.ts should render and match snapshot with data 1`] = `
           <div role="button"
                aria-haspopup="listbox"
                aria-expanded="false"
-               aria-owns="list-18"
+               aria-owns="list-17"
                class="v-input__slot"
           >
             <div class="v-select__slot">
@@ -125,7 +121,7 @@ exports[`VDataIterator.ts should render and match snapshot with data 1`] = `
                   10
                 </div>
                 <input aria-label="$vuetify.dataFooter.itemsPerPageText"
-                       id="input-18"
+                       id="input-17"
                        readonly="readonly"
                        type="text"
                        aria-readonly="true"
@@ -145,10 +141,6 @@ exports[`VDataIterator.ts should render and match snapshot with data 1`] = `
               >
             </div>
             <div class="v-menu">
-              <div class="v-menu__content theme--light "
-                   style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-              >
-              </div>
             </div>
           </div>
         </div>
@@ -202,7 +194,7 @@ exports[`VDataIterator.ts should render valid no-data, loading and no-results st
           <div role="button"
                aria-haspopup="listbox"
                aria-expanded="false"
-               aria-owns="list-31"
+               aria-owns="list-29"
                class="v-input__slot"
           >
             <div class="v-select__slot">
@@ -211,7 +203,7 @@ exports[`VDataIterator.ts should render valid no-data, loading and no-results st
                   10
                 </div>
                 <input aria-label="$vuetify.dataFooter.itemsPerPageText"
-                       id="input-31"
+                       id="input-29"
                        readonly="readonly"
                        type="text"
                        aria-readonly="true"
@@ -231,10 +223,6 @@ exports[`VDataIterator.ts should render valid no-data, loading and no-results st
               >
             </div>
             <div class="v-menu">
-              <div class="v-menu__content theme--light "
-                   style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-              >
-              </div>
             </div>
           </div>
         </div>
@@ -288,7 +276,7 @@ exports[`VDataIterator.ts should render valid no-data, loading and no-results st
           <div role="button"
                aria-haspopup="listbox"
                aria-expanded="false"
-               aria-owns="list-31"
+               aria-owns="list-29"
                class="v-input__slot"
           >
             <div class="v-select__slot">
@@ -297,7 +285,7 @@ exports[`VDataIterator.ts should render valid no-data, loading and no-results st
                   10
                 </div>
                 <input aria-label="$vuetify.dataFooter.itemsPerPageText"
-                       id="input-31"
+                       id="input-29"
                        readonly="readonly"
                        type="text"
                        aria-readonly="true"
@@ -317,10 +305,6 @@ exports[`VDataIterator.ts should render valid no-data, loading and no-results st
               >
             </div>
             <div class="v-menu">
-              <div class="v-menu__content theme--light "
-                   style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-              >
-              </div>
             </div>
           </div>
         </div>
@@ -374,7 +358,7 @@ exports[`VDataIterator.ts should render valid no-data, loading and no-results st
           <div role="button"
                aria-haspopup="listbox"
                aria-expanded="false"
-               aria-owns="list-31"
+               aria-owns="list-29"
                class="v-input__slot"
           >
             <div class="v-select__slot">
@@ -383,7 +367,7 @@ exports[`VDataIterator.ts should render valid no-data, loading and no-results st
                   10
                 </div>
                 <input aria-label="$vuetify.dataFooter.itemsPerPageText"
-                       id="input-31"
+                       id="input-29"
                        readonly="readonly"
                        type="text"
                        aria-readonly="true"
@@ -403,10 +387,6 @@ exports[`VDataIterator.ts should render valid no-data, loading and no-results st
               >
             </div>
             <div class="v-menu">
-              <div class="v-menu__content theme--light "
-                   style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-              >
-              </div>
             </div>
           </div>
         </div>

packages/vuetify/src/components/VDataTable/__tests__/__snapshots__/VDataTableHeader.spec.ts.snap
@@ -556,10 +556,6 @@ exports[`VDataTableHeader.ts mobile should render 1`] = `
                 <input type="hidden">
               </div>
               <div class="v-menu">
-                <div class="v-menu__content theme--light "
-                     style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-                >
-                </div>
               </div>
             </div>
           </div>
@@ -591,18 +587,18 @@ exports[`VDataTableHeader.ts mobile should render with data-table-select header 
             <div role="button"
                  aria-haspopup="listbox"
                  aria-expanded="false"
-                 aria-owns="list-79"
+                 aria-owns="list-74"
                  class="v-input__slot"
             >
               <div class="v-select__slot">
-                <label for="input-79"
+                <label for="input-74"
                        class="v-label theme--light"
                        style="left: 0px; position: absolute;"
                 >
                   Sort by
                 </label>
                 <div class="v-select__selections">
-                  <input id="input-79"
+                  <input id="input-74"
                          readonly="readonly"
                          type="text"
                          aria-readonly="true"
@@ -621,10 +617,6 @@ exports[`VDataTableHeader.ts mobile should render with data-table-select header 
                 <input type="hidden">
               </div>
               <div class="v-menu">
-                <div class="v-menu__content theme--light "
-                     style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-                >
-                </div>
               </div>
             </div>
           </div>
@@ -645,11 +637,11 @@ exports[`VDataTableHeader.ts mobile should work with multiSort 1`] = `
             <div role="button"
                  aria-haspopup="listbox"
                  aria-expanded="false"
-                 aria-owns="list-51"
+                 aria-owns="list-49"
                  class="v-input__slot"
             >
               <div class="v-select__slot">
-                <label for="input-51"
+                <label for="input-49"
                        class="v-label v-label--active theme--light"
                        style="left: 0px; position: absolute;"
                 >
@@ -669,7 +661,7 @@ exports[`VDataTableHeader.ts mobile should work with multiSort 1`] = `
                       </div>
                     </span>
                   </span>
-                  <input id="input-51"
+                  <input id="input-49"
                          readonly="readonly"
                          type="text"
                          aria-readonly="true"
@@ -690,10 +682,6 @@ exports[`VDataTableHeader.ts mobile should work with multiSort 1`] = `
                 >
               </div>
               <div class="v-menu">
-                <div class="v-menu__content theme--light "
-                     style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-                >
-                </div>
               </div>
             </div>
           </div>
@@ -714,18 +702,18 @@ exports[`VDataTableHeader.ts mobile should work with showGroupBy 1`] = `
             <div role="button"
                  aria-haspopup="listbox"
                  aria-expanded="false"
-                 aria-owns="list-44"
+                 aria-owns="list-43"
                  class="v-input__slot"
             >
               <div class="v-select__slot">
-                <label for="input-44"
+                <label for="input-43"
                        class="v-label theme--light"
                        style="left: 0px; position: absolute;"
                 >
                   Sort by
                 </label>
                 <div class="v-select__selections">
-                  <input id="input-44"
+                  <input id="input-43"
                          readonly="readonly"
                          type="text"
                          aria-readonly="true"
@@ -744,10 +732,6 @@ exports[`VDataTableHeader.ts mobile should work with showGroupBy 1`] = `
                 <input type="hidden">
               </div>
               <div class="v-menu">
-                <div class="v-menu__content theme--light "
-                     style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-                >
-                </div>
               </div>
             </div>
           </div>
@@ -768,11 +752,11 @@ exports[`VDataTableHeader.ts mobile should work with sortBy correctly 1`] = `
             <div role="button"
                  aria-haspopup="listbox"
                  aria-expanded="false"
-                 aria-owns="list-60"
+                 aria-owns="list-57"
                  class="v-input__slot"
             >
               <div class="v-select__slot">
-                <label for="input-60"
+                <label for="input-57"
                        class="v-label v-label--active theme--light"
                        style="left: 0px; position: absolute;"
                 >
@@ -792,7 +776,7 @@ exports[`VDataTableHeader.ts mobile should work with sortBy correctly 1`] = `
                       </div>
                     </span>
                   </span>
-                  <input id="input-60"
+                  <input id="input-57"
                          readonly="readonly"
                          type="text"
                          aria-readonly="true"
@@ -813,10 +797,6 @@ exports[`VDataTableHeader.ts mobile should work with sortBy correctly 1`] = `
                 >
               </div>
               <div class="v-menu">
-                <div class="v-menu__content theme--light "
-                     style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-                >
-                </div>
               </div>
             </div>
           </div>
@@ -837,11 +817,11 @@ exports[`VDataTableHeader.ts mobile should work with sortDesc correctly 1`] = `
             <div role="button"
                  aria-haspopup="listbox"
                  aria-expanded="false"
-                 aria-owns="list-69"
+                 aria-owns="list-65"
                  class="v-input__slot"
             >
               <div class="v-select__slot">
-                <label for="input-69"
+                <label for="input-65"
                        class="v-label v-label--active theme--light"
                        style="left: 0px; position: absolute;"
                 >
@@ -861,7 +841,7 @@ exports[`VDataTableHeader.ts mobile should work with sortDesc correctly 1`] = `
                       </div>
                     </span>
                   </span>
-                  <input id="input-69"
+                  <input id="input-65"
                          readonly="readonly"
                          type="text"
                          aria-readonly="true"
@@ -882,10 +862,6 @@ exports[`VDataTableHeader.ts mobile should work with sortDesc correctly 1`] = `
                 >
               </div>
               <div class="v-menu">
-                <div class="v-menu__content theme--light "
-                     style="max-height: 304px; min-width: 0px; max-width: auto; top: 12px; left: 0px; transform-origin: top left; z-index: 0; display: none;"
-                >
-                </div>
               </div>
             </div>
           </div>

commit 132cc52cf34b3fff3983cb7346b0aec85e676d43

fix(VDialog): allow more than 1 content node
theme provider only allows a single node, structure changed in 2b975f5 broke multiple nodes

packages/vuetify/src/components/VDialog/VDialog.ts
@@ -286,15 +286,13 @@ export default baseMixins.extend({
         style: { zIndex: this.activeZIndex },
         ref: 'content',
       }, [
-        this.$createElement('div', data, [
-          this.$createElement(VThemeProvider, {
-            props: {
-              root: true,
-              light: this.light,
-              dark: this.dark,
-            },
-          }, this.getContentSlot()),
-        ]),
+        this.$createElement(VThemeProvider, {
+          props: {
+            root: true,
+            light: this.light,
+            dark: this.dark,
+          },
+        }, [this.$createElement('div', data, this.getContentSlot())]),
       ])
     },
   },